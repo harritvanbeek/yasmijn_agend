6d39cc4585ddc0d94812c1a41d57e11a531e72aa
yasmijnofficial <131432856+yasmijnofficial@users.noreply.github.com>
2023-05-30 15:27:25 +0200

    Add files via upload

--- libraries/classes/core/dates.php
+++ libraries/classes/core/dates.php
@@ -7,16 +7,67 @@ class dates {
     
     public function translateDays(){
         $this->array = [
-            "Tuesday" => "Dinsdag",
-            "Wensday" => "Woensdag",
+            "Monday"            => "Maandag",
+            "Tuesday"           => "Dinsdag",
+            "Wensday"           => "Woensdag",
+            "Thursday"          => "Donderdag",
+            "Friday"            => "Vrijdag",
+            "Saturday"          => "Zaterdag",
+            "Sunday"            => "Zondag",
         ];
 
         return $this->array;
     }
 
     public function translateMonth(){
-        
+        $this->array = [
+            "January"               => "Januari",
+            "February"              => "Februari",
+            "March"                 => "Maart",
+            "April"                 => "April",
+            "May"                   => "Mei",
+            "June"                  => "Juni",
+            "July"                  => "Juli",
+            "August"                => "Augustus",
+            "September"             => "September",
+            "October"               => "Oktober",
+            "November"              => "November",
+            "December"              => "December",
+        ];
+
+        return $this->array;
     }
+        //afkortingen
+    public function translateDays_short(){
+            $this->array = [
+                "Mon"            => "Ma",
+                "Tue"           => "Di",
+                "Wed"           => "Wo",
+                "Thu"          => "Do",
+                "Fri"            => "Vr",
+                "Sat"          => "Za",
+                "Sun"            => "Zo",
+            ];
+    
+            return $this->array;
+    }
+    
+    public function translateMonth_short(){
+            $this->array = [
+                "Jan"               => "Jan",
+                "Feb"              => "Feb",
+                "Mar"                 => "Mrt",
+                "Apr"                 => "Apr",
+                "May"                   => "Mei",
+                "June"                  => "Jun",
+                "July"                  => "Jul",
+                "Aug"                => "Aug",
+                "Sep"             => "Sept",
+                "Oct"               => "Okt",
+                "Nov"              => "Nov",
+                "Dec"              => "Dec",
+            ];
 
-
+        return $this->array;
+    }
 }
