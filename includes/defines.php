afb73d67557e9a2ba3d3bf43d9c9086d345b692f
yasmijnofficial <131432856+yasmijnofficial@users.noreply.github.com>
2023-05-31 19:14:25 +0200

    Update defines.php

--- includes/defines.php
+++ includes/defines.php
@@ -7,20 +7,32 @@
     $parts      = explode(DIRECTORY_SEPARATOR, BPATH_BASE);
 
     
-    if($_SERVER["SERVER_NAME"] !== "boann.home"){        
+    if($_SERVER["SERVER_NAME"] === "boann.home"){        
         $home = "/yasmijn_agend";
-    }elseif($_SERVER["SERVER_NAME"] !== "agenda.yasmijnvels.nl"){
-        $home = "/yasmijn_agend";
-    }
 
-    define('DS',                  DIRECTORY_SEPARATOR);
-    define('BOANN_ROOT',          implode(DS, $parts));
-    define('BOANN_SITE',          BOANN_ROOT);
-    define('USER_IP',             $_SERVER["REMOTE_ADDR"]);
-    define('BOANN_LIBRARIES',     BOANN_ROOT . DS . 'libraries');
-    define('BOANN_THEMES',        BPATH_BASE . DS . 'templates');
-    define('BOANN_CACHE',         BPATH_BASE . DS . 'cache');
-    define('BPATH_CONFIGURATION', BPATH_BASE . DS);
-    define('SITE',                "//{$_SERVER["SERVER_NAME"]}{$home}");    
-    define('ASSETS',              SITE."/assets");  
-    define('THEMES',              SITE."/templates");     
+        define('DS',                  DIRECTORY_SEPARATOR);
+        define('BOANN_ROOT',          implode(DS, $parts));
+        define('BOANN_SITE',          BOANN_ROOT);
+        define('USER_IP',             $_SERVER["REMOTE_ADDR"]);
+        define('BOANN_LIBRARIES',     BOANN_ROOT . DS . 'libraries');
+        define('BOANN_THEMES',        BPATH_BASE . DS . 'templates');
+        define('BOANN_CACHE',         BPATH_BASE . DS . 'cache');
+        define('BPATH_CONFIGURATION', BPATH_BASE . DS);
+        define('SITE',                "//{$_SERVER["SERVER_NAME"]}{$home}");    
+        define('ASSETS',              SITE."/assets");  
+        define('THEMES',              SITE."/templates");  
+    }elseif($_SERVER["SERVER_NAME"] === "agenda.yasmijnvels.nl"){
+        $home = "/yasmijn_agend";
+
+        define('DS',                  DIRECTORY_SEPARATOR);
+        define('BOANN_ROOT',          implode(DS, $parts));
+        define('BOANN_SITE',          BOANN_ROOT);
+        define('USER_IP',             $_SERVER["REMOTE_ADDR"]);
+        define('BOANN_LIBRARIES',     BOANN_ROOT . DS . 'libraries');
+        define('BOANN_THEMES',        BPATH_BASE . DS . 'templates');
+        define('BOANN_CACHE',         BPATH_BASE . DS . 'cache');
+        define('BPATH_CONFIGURATION', BPATH_BASE . DS);
+        define('SITE',                "//{$_SERVER["SERVER_NAME"]}");    
+        define('ASSETS',              SITE."/assets");  
+        define('THEMES',              SITE."/templates");  
+    }   
